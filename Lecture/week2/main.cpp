d70a5d3dc12d574dbfe985acc901dc4baa562e4f
Jason <jwest@coloradomesa.edu>
2024-01-29 17:03:38 -0700

    variable examples

--- Lecture/week2/main.cpp
+++ Lecture/week2/main.cpp
@@ -1,6 +1,14 @@
-#include <iostream> 
+#include <iostream>
+#include <string.h>
 
 int main() {
-
+    int blah;
+    blah = 400;
+    std::string myString;
+    myString = "anything";
+    float blahFloat = 45.5;
+    std::cout << "blah = " << blah << std::endl;
+    std::cout << "myString = " << myString << std::endl;
+    std::cout << "blahFloat = " << blahFloat << std::endl;
     return 0;
 }
