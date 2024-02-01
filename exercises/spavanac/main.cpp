4e1f5d6300476c7b680bf1cbbd701c697d6291c3
Jason <jwest@coloradomesa.edu>
2024-02-01 16:06:08 -0700

    Initial spavanac

---
+++ exercises/spavanac/main.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream> //library for input and output
+#include <string> //library for string data
+ 
+using namespace std; //resolve cout, cin, and endl names
+ 
+//main entry point of the program
+int main() {
+    
+}
