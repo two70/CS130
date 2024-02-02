b427a9e035d0d55fe268f5e07fe7cd59958c5340
Jason <jwest@coloradomesa.edu>
2024-02-01 17:10:56 -0700

    spavanac completed

--- exercises/spavanac/main.cpp
+++ exercises/spavanac/main.cpp
@@ -6,5 +6,20 @@ using namespace std; //resolve cout, cin, and endl names
  
 //main entry point of the program
 int main() {
-    
+    int H = 0; 
+    int M = 0;
+    cout << "Enter two numbers seperated by space" << endl;
+    cin >> H >> M;
+    // cout << H << " " << M << endl;
+    int totalMinutes = 0;
+    totalMinutes = (H * 60) + M;
+    // cout << totalMinutes << endl;
+    totalMinutes = totalMinutes - 45;
+    if(totalMinutes < 0) {
+        totalMinutes = 1440 + totalMinutes;
+    }
+    H = totalMinutes / 60;
+    M = totalMinutes - (H * 60);
+    cout << "Hours = " << H << " Minutes = " << M << endl;
+
 }
