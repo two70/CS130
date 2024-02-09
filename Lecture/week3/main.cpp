cea60d2e7b2f930c70cfa98753a19eb2c3f48dec
Jason <jwest@coloradomesa.edu>
2024-02-09 15:52:54 -0700

    lecture notes

--- Lecture/week3/main.cpp
+++ Lecture/week3/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <string>
+#include <stdlib.h>
+
+using namespace std;
+
+enum COLOR {RED = 22, BLUE = 11, GREEN, YELLOW, PURPLE};
+
+namespace MY_SPACE {
+    const float PI = 3.14156;
+    const double G = 6.67384e-11; // gravitional force in cubic meter per second squared per kilogram
+    const double c = 2.99792458e8; // speed of light in vacuum in meter per second
+    string first_name = "Jake";
+    string last_name = "Smith";
+}
+
+using namespace MY_SPACE;
 
 int main() {
-
+    string myobject;
+    myobject = to_string(189);
+    cout << myobject << endl;
     return 0;
 }
