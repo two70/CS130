177c5a838b142e0a8fe03e92d416069b8fc0184a
Jason <jwest@coloradomesa.edu>
2024-02-27 16:02:46 -0700

    week6 lecture

--- Lecture/week6/main.cpp
+++ Lecture/week6/main.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 int main() {
+    string my_string = "this is a  \t     sentance";
 
+    for(char c: my_string) {
+        if(int(c) == 32 or int(c) == 9){
+            continue;
+        }
+        cout << c << " is " << int(c) << "\t";
+    }
     return 0;
 }
