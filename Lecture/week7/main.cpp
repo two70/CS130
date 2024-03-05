bf0fffdf7e303e9abc362b2a9f2facd763d7c92c
Jason <jwest@coloradomesa.edu>
2024-03-05 16:10:33 -0700

    updated week7 lecture

--- Lecture/week7/main.cpp
+++ Lecture/week7/main.cpp
@@ -2,6 +2,24 @@
 
 using namespace std;
 int main() {
+    // int ourInteger = 10;
+    // int* pOurInteger = &ourInteger;
+    // cout << ourInteger << endl;
+    // cout << &ourInteger << endl;
+    // cout << pOurInteger << endl;
+    // cout << *pOurInteger << endl;
 
+    // *pOurInteger += 1;
+    // pOurInteger += 1;
+    // cout << pOurInteger << endl;
+    // cout << *pOurInteger << endl;
+
+    // int* pNumb1 = new int;
+    // int* pOurInteger;
+
+    // cout << pNumb1 << endl;
+
+    // delete pNumb1;
+    // cout << pNumb1 << endl;
     return 0;
 }
