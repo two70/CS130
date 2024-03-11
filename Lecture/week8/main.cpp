915a86f69bea543eabbd0ede4cfae538b6b61be5
Jason <jwest@coloradomesa.edu>
2024-03-11 16:40:55 -0600

    week 8 lecture

--- Lecture/week8/main.cpp
+++ Lecture/week8/main.cpp
@@ -2,6 +2,25 @@
 
 using namespace std;
 int main() {
+    // int * dynArray = new int[3];
+    // dynArray[0] = 10;
 
+    // cout << dynArray[0] << endl;
+    int number = 0;
+
+    cout << "How many values:\n";
+    cin >> number;
+
+    int *dynArray = new int[number];
+
+    for(int i = 0; i < number; i++ ) {
+        cout << "Enter value for " << i << endl;
+        cin >> dynArray[i];
+    }
+
+    for(int i = 0; i < number; i++) {
+        cout << i << " is " << dynArray[i] << endl;
+    }
+    delete [] dynArray;
     return 0;
 }
