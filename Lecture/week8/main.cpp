ea1d7bdcae49a76fe981f390150c2bd4fd773d4a
Jason <jwest@coloradomesa.edu>
2024-03-13 16:18:46 -0600

    other things

--- Lecture/week8/main.cpp
+++ Lecture/week8/main.cpp
@@ -2,25 +2,31 @@
 
 using namespace std;
 int main() {
-    // int * dynArray = new int[3];
-    // dynArray[0] = 10;
+    int ** rowArray = new int[3];
+    int * colArray = new int[3];
+    rowArray[0] = colArray;
 
     // cout << dynArray[0] << endl;
-    int number = 0;
+    // int number = 0;
 
-    cout << "How many values:\n";
-    cin >> number;
+    // cout << "How many values:\n";
+    // cin >> number;
 
-    int *dynArray = new int[number];
+    // int *dynArray = new int[number];
 
-    for(int i = 0; i < number; i++ ) {
-        cout << "Enter value for " << i << endl;
-        cin >> dynArray[i];
-    }
+    // for(int i = 0; i < number; i++ ) {
+    //     cout << "Enter value for " << i << endl;
+    //     cin >> dynArray[i];
+    // }
 
-    for(int i = 0; i < number; i++) {
-        cout << i << " is " << dynArray[i] << endl;
-    }
-    delete [] dynArray;
+    // for(int i = 0; i < number; i++) {
+    //     cout << i << " is " << dynArray[i] << endl;
+    // }
+    // delete [] dynArray;
+
+    // int board[3][3];
+    // cout << "whole board " << sizeof(board) << endl;
+    // cout << "row " << sizeof(board[0]) << endl;
+    // cout << "column " << sizeof(board[0][0]) << endl;
     return 0;
 }
