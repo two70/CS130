a14721483c75141eed8577e90a54f65d00c6005d
Jason <jwest@coloradomesa.edu>
2024-03-26 16:34:41 -0600

    Binary and back

--- Lecture/week9/main.cpp
+++ Lecture/week9/main.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
-int main() {
 
+string binary(unsigned int decimal) {
+    const int divisor = 2;
+    int dividend;
+    int quotient, remain;
+    string answer = "";
+    quotient = decimal;
+
+    while(quotient != 0) {
+        dividend = quotient;
+        remain = dividend % divisor;
+        quotient = dividend / divisor;
+
+        answer = to_string(remain) + answer;
+    }
+
+    if (answer.empty())
+        return "0";
+
+    return answer;
+}
+
+unsigned int decimal(string binary) {
+    int answer = 0;
+    int digitCount = binary.size();
+    for(int i = 0; i < digitCount; i++){
+        if (binary[i] == '0') continue;
+        int placeValue = digitCount - i - 1;
+        answer += pow(2.0, placeValue);
+    }
+    return answer;
+}
+
+int main() {
+    // cout << binary(100) << endl;
+    cout << decimal(binary(100)) << endl;
     return 0;
 }
