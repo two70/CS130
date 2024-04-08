62a2e7f5eba145dffb9f5314b4676ca2b256cc43
Jason <jwest@coloradomesa.edu>
2024-04-08 16:49:26 -0600

    lecture 11

--- Lecture/week11/main.cpp
+++ Lecture/week11/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 int main() {
 
+    string file_path = "./inputfile.txt";
+    string data = "";
+    fstream file;
+    file.open(file_path, file.in | file.binary | file.ate);
+    if (!file.is_open()) {
+        cout << "file BAD" << endl;
+    }
+    else {
+        cout << "size = " << file.tellg() << endl;
+        getline(file, data);
+        cout << "data = " << data << endl;
+    }
+    file.close();
     return 0;
 }
